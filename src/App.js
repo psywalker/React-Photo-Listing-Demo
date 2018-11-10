9fa51b4bcb3b89536f72a6d852f409ca7abff525
psywalker <psywalker09@gmail.com>
2018-11-10 19:56:57 +0300

    I create field of images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {FormInline, Fa, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="row">
+          <div className="col-8">
+            <div class="flex-container">
+              <a href="#" class="logo">LOGO</a>
+              <FormInline className="md-form">
+                <Fa icon="search" />
+                <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search"/>
+              </FormInline>
+            </div>
+          </div>
+          <div className="col-4">
+
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-8">      
+            <Dropdown>
+              <DropdownToggle caret color="primary">
+              Material dropdown
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem href="#">Action</DropdownItem>
+                <DropdownItem href="#">Another Action</DropdownItem>
+                <DropdownItem href="#">Something else here</DropdownItem>
+                <DropdownItem href="#">Something else here</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+          <div className="col-4">
+
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">      
+            <ul class="photo-list">
+              <li class="photo-list__item">
+                <a href="#" class="photo-list__link">
+                  <img src="./images/avenue.jpg" class="photo-list__img" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-4">
+
+          </div>
+        </div>
       </div>
     );
   }
