7abb647ac8eb6e4dca73c0683f26d01c8919b393
psywalker <musachev@voximplant.com>
2018-11-12 19:45:32 +0300

    Filter: Make array of filters #6

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,36 +8,21 @@ export class Filter extends Component {
     state = {
         activeFilter: 'filter1',
         showDropdown: false,
+        filters: ['filter1', 'filter2', 'filter3', 'filter4']
     };
 
     render() {
+        const listItems = this.state.filters.map((item,i) => {
+            return <DropdownItem key={i} activeFilter={this.state.activeFilter} filterType={item} onFilterClick={() => this.setState({activeFilter: {item}, showDropdown: false})}/>});
+        console.log('listItems::', listItems)
         return (
             <Dropdown className="ml-3">
                 <DropdownToggle caret color="primary ml-0" onClick={() => this.setState({showDropdown: true})}>
                     {this.state.activeFilter}
                 </DropdownToggle>
-                {this.state.showDropdown && <DropdownMenu style={{display: 'block'}} >
-                    <DropdownItem
-                        {...this.state}
-                        filterType="filter1"
-                        onFilterClick={() => this.setState({activeFilter: "filter1", showDropdown: false})}
-                    />
-                    <DropdownItem
-                        {...this.state}
-                        filterType="filter2"
-                        onFilterClick={() => this.setState({activeFilter: "filter2", showDropdown: false})}
-                    />
-                    <DropdownItem
-                        {...this.state}
-                        filterType="filter3"
-                        onFilterClick={() => this.setState({activeFilter: "filter3", showDropdown: false})}
-                    />
-                    <DropdownItem
-                        {...this.state}
-                        filterType="filter4"
-                        onFilterClick={() => this.setState({activeFilter: "filter4", showDropdown: false})}
-                    />
-                </DropdownMenu>}
+                <DropdownMenu>
+                    {listItems}
+                </DropdownMenu>
             </Dropdown>
         );
     }
