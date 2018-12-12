d2d8bb2965cb2b8204710c508b22773792d46abc
psywalker <musachev@voximplant.com>
2018-12-12 11:57:55 +0300

    Added load status for Photo component #46

--- src/pages/Photo/index.js
+++ src/pages/Photo/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ButtonBack from '../../components/ButtonBack';
+import Spinner from '../../components/Spinner';
 import {
     Card,
     CardBody,
@@ -14,6 +15,7 @@ class Photo extends Component {
     constructor(...args) {
       super(...args);
       this.state = {
+        isListingLoading: false,
         photoSrc: null,
         userId: 'User',
         userName: 'UserName',
@@ -30,6 +32,7 @@ class Photo extends Component {
 
     handlePhotoQuery = () => {
         const { match, history } = this.props;
+        this.setState({ isListingLoading: true });
         const API_URL = `${process.env.REACT_APP_UNSPLASH_API_NAME}photos/${match.params.id}?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`;
 
         axios.get(API_URL)
@@ -43,6 +46,7 @@ class Photo extends Component {
             const photoCreated = res.data.created_at;
   
             this.setState({
+                isListingLoading: false,
                 photoSrc,
                 userId,
                 userName,
@@ -59,6 +63,7 @@ class Photo extends Component {
     }
     render() {
         const { 
+            isListingLoading,
             photoSrc,
             userName,
             userLastName,
@@ -67,20 +72,23 @@ class Photo extends Component {
          } = this.state;
 
         return (
-        <div className="photo">
-            <Card className="photo-card">
-                <CardImage className="img-fluid photo-card__img" src={photoSrc} />
-                <CardBody>
-                    <Link to={`/users/${userNic}`}>
-                        <h2>Autor: {userName} {userLastName}</h2>
-                    </Link>
-                    <a className="photo-card__autor-link" href={userPortfolioUrl}>Autor's portfolio link</a>
-                    <div>
-                        <ButtonBack />
-                    </div>
-                    
-                </CardBody>
-            </Card>
+        <div className="photo-container">
+            { isListingLoading && (<Spinner />)}
+            <div className="photo">
+                <Card className="photo-card">
+                    <CardImage className="img-fluid photo-card__img" src={photoSrc} />
+                    <CardBody>
+                        <Link to={`/users/${userNic}`}>
+                            <h2>Autor: {userName} {userLastName}</h2>
+                        </Link>
+                        <a className="photo-card__autor-link" href={userPortfolioUrl}>Autor's portfolio link</a>
+                        <div>
+                            <ButtonBack />
+                        </div>
+                        
+                    </CardBody>
+                </Card>
+            </div>
         </div>
         );
     }
