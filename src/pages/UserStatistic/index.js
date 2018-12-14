5ec809efd5ab4da1efada91dbd269ccc3cfaf33c
psywalker <musachev@voximplant.com>
2018-12-14 14:31:49 +0300

    Done user statistic component and add react-highcharts #51

--- src/pages/UserStatistic/index.js
+++ src/pages/UserStatistic/index.js
@@ -9,87 +9,81 @@ class UserStatistic extends Component {
       super(...args);
       this.state = {
         isListingLoading: false,
-        highchartsConfig: {
-            title: {
-                text: 'My statistic'
-            },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            series: [
-                {
-                    name: 'Downloads',
-                    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-                },
-                {
-                    name: 'Views',
-                    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-                },
-                {
-                    name: 'Likes',
-                    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-                },
-            ],
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
-            },
-            responsive: {
-                rules: [{
-                  condition: {
-                    maxWidth: 700
-                  },
-                  chartOptions: {
-                    legend: {
-                      layout: 'horizontal',
-                      align: 'center',
-                      verticalAlign: 'bottom'
-                    }
-                  }
-                }]
-              }
-        },
-        page: 1,
-        per_page: 6,
-      };
-    }
-
-    componentDidMount = () => {
-        this.handleUserPhotoListingQuery();
-    };
-
-    handleUserPhotoListingQuery = () => {
-        const { userId } = this.props;
-        const { page, per_page } = this.state;
-        this.setState({ isListingLoading: true });
-
-
-        console.log('333', `${process.env.REACT_APP_UNSPLASH_API_NAME}users/${userId}/statistics?${process.env.REACT_APP_UNSPLASH_API_KEY}`)
-        axios.get(`${process.env.REACT_APP_UNSPLASH_API_NAME}users/${userId}/statistics?`, {
-            params: {
-                page, 
-                per_page,
-                client_id: process.env.REACT_APP_UNSPLASH_API_KEY,
-            },
-        }).then((res) => {
-            console.log('999:::', res)
-            const highchartsConfig = {
+        highchartsConfigs: {
+            highchartsDownloadsConfig: {
                 title: {
-                    text: 'My statistic'
+                    text: 'My Downloads'
                 },
                 xAxis: {
-                    categories: []
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
                 },
                 series: [
                     {
                         name: 'Downloads',
                         data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
                     },
+                ],
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle'
+                },
+                responsive: {
+                    rules: [{
+                      condition: {
+                        maxWidth: 700
+                      },
+                      chartOptions: {
+                        legend: {
+                          layout: 'horizontal',
+                          align: 'center',
+                          verticalAlign: 'bottom'
+                        }
+                      }
+                    }]
+                  }
+            },
+            highchartsViewsConfig: {
+                title: {
+                    text: 'My Views'
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                series: [
                     {
                         name: 'Views',
                         data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
                     },
+                ],
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle'
+                },
+                responsive: {
+                    rules: [{
+                      condition: {
+                        maxWidth: 700
+                      },
+                      chartOptions: {
+                        legend: {
+                          layout: 'horizontal',
+                          align: 'center',
+                          verticalAlign: 'bottom'
+                        }
+                      }
+                    }]
+                  }
+            },
+            highchartsLikesConfig: {
+                title: {
+                    text: 'My Likes'
+                },
+                xAxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                },
+                series: [
                     {
                         name: 'Likes',
                         data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
@@ -114,23 +108,66 @@ class UserStatistic extends Component {
                       }
                     }]
                   }
-            }
-            console.log('000:::', res.data.downloads.historical.values)
-            highchartsConfig.xAxis.categories = res.data.downloads.historical.values.map(function(item) {
-                return item.date;
-            });
-            highchartsConfig.series[0].data = res.data.downloads.historical.values.map(function(item) {
-                return item.value;
-            });
-            highchartsConfig.series[1].data = res.data.views.historical.values.map(function(item) {
-                return item.value;
-            });
-            highchartsConfig.series[2].data = res.data.likes.historical.values.map(function(item) {
-                return item.value;
-            });
+            },
+        },
+      };
+    }
 
+    componentDidMount = () => {
+        this.handleUserPhotoListingQuery();
+    };
+
+    handleUserPhotoListingQuery = () => {
+        const { userId } = this.props;
+        const { highchartsConfigs } = this.state;
+        this.setState({ isListingLoading: true });
+
+        axios.get(`${process.env.REACT_APP_UNSPLASH_API_NAME}users/${userId}/statistics?`, {
+            params: {
+                client_id: process.env.REACT_APP_UNSPLASH_API_KEY,
+            },
+        }).then((res) => {
+
+            const highchartsConfigsObject = {
+                highchartsDownloadsConfig: {
+                    ...highchartsConfigs.highchartsDownloadsConfig,
+                    xAxis: {
+                        categories: res.data.downloads.historical.values.map((item) => item.date)
+                    },
+                    series: [
+                        {
+                            ...highchartsConfigs.highchartsDownloadsConfig.series[0],
+                            data: res.data.downloads.historical.values.map((item) => item.value),
+                        }
+                    ] 
+                },
+                highchartsViewsConfig: {
+                    ...highchartsConfigs.highchartsViewsConfig,
+                    xAxis: {
+                        categories: res.data.views.historical.values.map((item) => item.date),
+                    },
+                    series: [
+                        {
+                            ...highchartsConfigs.highchartsViewsConfig.series[0],
+                            data: res.data.views.historical.values.map((item) => item.value),
+                        }
+                    ] 
+                },
+                highchartsLikesConfig: {
+                    ...highchartsConfigs.highchartsLikesConfig,
+                    xAxis: {
+                        categories: res.data.likes.historical.values.map((item) => item.date),
+                    },
+                    series: [
+                        {
+                            ...highchartsConfigs.highchartsDownloadsConfig.series[0],
+                            data: res.data.likes.historical.values.map((item) => item.value),
+                        }
+                    ] 
+                },
+            }
             this.setState({
-                highchartsConfig,
+                highchartsConfigs: highchartsConfigsObject,
                 isListingLoading: false,
             }); 
         })
@@ -141,13 +178,23 @@ class UserStatistic extends Component {
     render() {
         const {
             isListingLoading,
-            highchartsConfig,
+            highchartsConfigs,
         } = this.state;
         return (
             <div className="user-statistic">
                 { isListingLoading && (<Spinner />)}
                 <div className="user-statistic__charts">
-                    <Highcharts config = {highchartsConfig}></Highcharts>
+                    <div className="user-statistic__chart-wrap">
+                        <Highcharts  className="user-statistic__chart" config = {highchartsConfigs.highchartsDownloadsConfig}></Highcharts>
+                    </div>
+
+                    <div className="user-statistic__chart-wrap">
+                        <Highcharts  className="user-statistic__chart" config = {highchartsConfigs.highchartsViewsConfig}></Highcharts>
+                    </div>
+
+                    <div className="user-statistic__chart-wrap">
+                        <Highcharts  className="user-statistic__chart" config = {highchartsConfigs.highchartsLikesConfig}></Highcharts>
+                    </div>
                 </div>
             </div>
         );
