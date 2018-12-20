f39fad5181a56ec96ee4cda99edae9d9f2e6b2a0
psywalker <musachev@voximplant.com>
2018-12-20 14:36:58 +0300

    Tested component PhotoCard #54

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -13,10 +13,6 @@ class PhotoCard extends Component {
     };
   }
 
-  componentDidMount = () => {
-   
-  };
-
   showModal = () => {
     this.setState({
       visible: true,
@@ -70,7 +66,7 @@ class PhotoCard extends Component {
           <div className="photo-card-self__badge-wrap">
             {tags.map((item, i) => {
                 if(i < 3) {
-                  return <Tag onClick={() => onSearchTagValue(item.title, 'tags')} className="photo-card-self__badge" key={item.title}>
+                  return <Tag key={item.title} onClick={() => onSearchTagValue(item.title, 'tags')} className="photo-card-self__badge" key={item.title}>
                       {item.title}
                     </Tag>
                   }else {
@@ -86,7 +82,7 @@ class PhotoCard extends Component {
                                       <div>
                                           {tags.map((item, i) => {
                                             if(i > 2) {
-                                              return <Tag onClick={() => onSearchTagValue(item.title, 'tags')} className="photo-card-self__badge" key={item.title}>
+                                              return <Tag key={item.title} onClick={() => onSearchTagValue(item.title, 'tags')} className="photo-card-self__badge" key={item.title}>
                                                   {item.title}
                                                 </Tag>
                                               }else {
