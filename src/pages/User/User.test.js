9968cc668a4f79868ed52681db03184f88f1f018
psywalker <musachev@voximplant.com>
2018-12-20 12:46:12 +0300

    Tested component User #54

--- src/pages/User/User.test.js
+++ src/pages/User/User.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Link, Route } from 'react-router-dom';
 import User from '.';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Test of component of User', () => {
-  it('Initial', () => { 
+  it('Test User snapshot', () => { 
     const props = {
       match: {
         params: {
@@ -17,17 +16,7 @@ describe('Test of component of User', () => {
     }
 
     const user = shallow(<User {...props} />);
-    user.setState({ 
-      userName: 'User111', 
-      userLastName: 'UserLastName',
-      userPortfolioUrl: 'userPortfolioUrl',
-      userFirstPhoto: 'userFirstPhotoSrc',
-      userPhoto: 'userPhotoSrc'
-    }); 
-    //expect(user.find('.user-card__title').text()).toEqual("User111 UserLastName");
-    //expect(user.find('.user-card__first-img').prop('src')).toEqual("userFirstPhotoSrc");
-    //expect(user.find('.user-card__photo').prop('src')).toEqual("userPhotoSrc");
-    //expect(user.find('.user-card__link-portfolio').prop('href')).toEqual("userPortfolioUrl");
-    //expect(user).toMatchSnapshot();
+
+    expect(user).toMatchSnapshot();
   });
 });
