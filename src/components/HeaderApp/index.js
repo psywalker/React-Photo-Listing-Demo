982ea28ad6119414d476fbaae05546850548443c
psywalker <musachev@voximplant.com>
2019-01-14 16:03:19 +0300

    Processed request response #56

--- src/components/HeaderApp/index.js
+++ src/components/HeaderApp/index.js
@@ -11,10 +11,16 @@ class HeaderApp extends Component {
     constructor(...args) {
       super(...args);
       this.state = {
-        scopes: 'scope=public+read_user+write_user+read_photos+write_likes+write_photos+write_followers+read_collections+write_collections'
+        scopes: 'public+read_user+write_user+read_photos+write_likes+write_photos+write_followers+read_collections+write_collections'
       };
     }
-    
+
+    componentDidMount = () => {
+      /*this.setState({
+        scopes: 'public+read_user+write_user+read_photos+write_likes+write_photos+write_followers+read_collections+write_collections',
+      });*/
+    };
+
     render() {
       const { scopes } = this.state.scopes;
       return (
@@ -23,7 +29,7 @@ class HeaderApp extends Component {
                 <div className="page">
                     <Link to={`/`}> <Icon component={() => <img className="user__icon-home" alt="pixabay.com" src="https://www.vectorlogo.zone/logos/pixabay/pixabay-card.png"/>} /> </Link> 
                     <Route path="/:id" component={() => <ButtonBack />} /> 
-                    <a href={`https://unsplash.com/oauth/authorize?redirect_uri=https://psywalker.github.io/React-Photo-Listing-Demo/profile&response_type=code&${scopes}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`}>Autorization link</a>
+                    <a href={`https://unsplash.com/oauth/authorize?redirect_uri=http://localhost:3000/profile/&response_type=code&scope=public+read_user+write_user+read_photos+write_likes+write_photos+write_followers+read_collections+write_collections&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`}>Autorization link</a>
                 </div>
             </Header>    
         </div>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,7 +5,7 @@ class Profile extends Component {
     constructor(...args) {
       super(...args);
       this.state = {
-        
+        token: null,
       };
     }
 
@@ -14,42 +14,51 @@ class Profile extends Component {
     };
 
     handleAuthorizationRequest = () => {
-        
-        let code = document.location.search.split('?code=')
-        console.log('444', code[1])
-        axios.post('https://unsplash.com/oauth/token', {
-            
-              redirect_uri: 'https://psywalker.github.io/React-Photo-Listing-Demo/profile', 
-              client_secret: '21b065299de3e2b21be2fec1090d4de4156c8d4c08c7174977c44c273f24842c',
-              code: code[1],
-              grant_type: 'authorization_code',
-              client_id: process.env.REACT_APP_UNSPLASH_API_KEY
-         
-          }).then((res) => {
-            console.log('111', res);
-          })
-          .catch(() => {
-            console.log('pixabay API not responding');
-            
-          });
+      let token = localStorage.getItem('token');
 
-          /*axios({
-            method: 'post',
-            url: 'https://unsplash.com/oauth/token?',
-            data: {
-                redirect_uri: '/', 
+      if (token) {
+        console.log('111', token);
+        let code = document.location.search.split('?code=')
+        let headers = {
+              'Authorization': 'Bearer ' + token, 
+        }
+        axios.get('https://unsplash.com/me/psywalker', {headers: headers}).then((res) => {
+          console.log('222', res);
+        
+        })
+        .catch(() => {
+          console.log('222', 'pixabay API not responding');
+          
+        });
+      } else {
+
+        let code = document.location.search.split('?code=')
+
+        if(code) {
+          axios.post('https://unsplash.com/oauth/token', {
+              
+                redirect_uri: 'http://localhost:3000/profile/', 
                 client_secret: '21b065299de3e2b21be2fec1090d4de4156c8d4c08c7174977c44c273f24842c',
                 code: code[1],
                 grant_type: 'authorization_code',
                 client_id: process.env.REACT_APP_UNSPLASH_API_KEY
-            }
-          }).then((res) => {
-            console.log('111', res);
-          })
-          .catch(() => {
-            console.log('pixabay API not responding');
-            
-          });*/
+          
+            }).then((res) => {
+              console.log('333', res);
+              const token = res.data.access_token;
+              localStorage.clear();
+              localStorage.setItem('token', token);
+              console.log('333', token, localStorage.getItem('token'));
+            })
+            .catch(() => {
+              console.log('333', 'pixabay API not responding');
+              
+            });
+        } else {
+          console.log('Предложить авторизацию');
+          /* здесь позже код напишу, пока надо разобраться с кодом выше */
+        }
+      } 
     }
 
     render() {
