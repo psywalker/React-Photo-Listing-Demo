b4e941a74e15a77469c6bd40f3dc15683721d6c5
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2019-05-28 18:54:52 +0300

    Settings of autorization

--- src/components/HeaderApp/index.js
+++ src/components/HeaderApp/index.js
@@ -16,7 +16,7 @@ class HeaderApp extends Component {
                 <div className="page">
                     <Link to={`/`}> <Icon component={() => <img className="user__icon-home" alt="pixabay.com" src="https://www.vectorlogo.zone/logos/pixabay/pixabay-card.png"/>} /> </Link> 
                     <Route path="/:id" component={() => <ButtonBack />} /> 
-                    <a href={`https://unsplash.com/oauth/authorize?redirect_uri=http://localhost:3000/profile&response_type=code&scope=public+read_user+write_user+read_photos+write_likes+write_photos+write_followers+read_collections+write_collections&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`}>Autorization link</a>                </div>
+                    <a href={`https://unsplash.com/oauth/authorize?redirect_uri=${process.env.REACT_APP_UNSPLASH_API_REDIRECT_URI}&response_type=code&scope=public+read_user+write_user+read_photos+write_likes+write_photos+write_followers+read_collections+write_collections&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`}>Autorization link</a>                </div>
             </Header>    
         </div>
       );
