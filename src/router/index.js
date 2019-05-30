cbee4d45bae979f1f38807cee63064474365ed6a
psywalker <psywalker09@gmail.com>
2019-05-30 16:10:50 +0300

    Install Prettier and put code in right #58

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { LastLocationProvider } from 'react-router-last-location';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { Row, Col, Layout } from "antd";
+import { Row, Col, Layout } from 'antd';
 import Home from '../pages/Home/Home';
 import Photo from '../pages/Photo';
 import User from '../pages/User';
@@ -10,25 +10,25 @@ import Profile from '../pages/Profile';
 import HeaderApp from '../components/HeaderApp';
 
 const Main = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
-        <LastLocationProvider>
-            <Row style={{margin: '0px 0 30px'}}>
-            <Col>
-                <Layout className="user-layout">
-                    <HeaderApp />
-                </Layout>
-            </Col>
-        </Row>
-        <div className="page">
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/users/:id/photos" component={UserPhotoListing}/>
-                <Route path="/photo/:id" component={Photo}/>
-                <Route path="/profile" component={Profile}/>
-                <Route path="/users/:id" component={User}/>
-            </Switch>
-        </div>
-      </LastLocationProvider>
-    </BrowserRouter>)
+  <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
+    <LastLocationProvider>
+      <Row style={{ margin: '0px 0 30px' }}>
+        <Col>
+          <Layout className="user-layout">
+            <HeaderApp />
+          </Layout>
+        </Col>
+      </Row>
+      <div className="page">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/users/:id/photos" component={UserPhotoListing} />
+          <Route path="/photo/:id" component={Photo} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/users/:id" component={User} />
+        </Switch>
+      </div>
+    </LastLocationProvider>
+  </BrowserRouter>)
 
-  export default Main;
+export default Main;
