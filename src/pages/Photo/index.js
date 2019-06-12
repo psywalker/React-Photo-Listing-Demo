ea46808c3252d58e0080343c143758f264bbd7a5
psywalker <psywalker09@gmail.com>
2019-06-12 15:18:07 +0300

    Make the component Photo functional (part 1) #65

--- src/pages/Photo/index.js
+++ src/pages/Photo/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -8,59 +8,106 @@ import { photoRequestAction } from '../../actions';
 import './photo.css';
 
 const { Meta } = Card;
+const Photo = memo(({
+  match,
+  photoRequestAction: handleAction,
+  isPhotoLoading,
+  requestError,
+  photoSrc,
+  photoDesc,
+  userNic,
+  userPortfolioUrl,
+}) => {
+  console.log("1:: ", match)
 
-class Photo extends PureComponent {
-  componentDidMount = () => {
-    const { match, photoRequestAction: handleAction } = this.props;
-    handleAction(match);
-  };
+  handleAction(match);
+  return (
+    <div className="photo-container photo">
+      { isPhotoLoading && (<Spinner className="spinner" />)}
+      { !isPhotoLoading && !requestError && (
+        <Card
+          style={{ width: '100%' }}
+          cover={(
+            <img
+              className="photo__img"
+              alt="example"
+              src={photoSrc}
+            />
+          )}
+        >
+          <Meta className="photo__desc" title={`${photoDesc || 'No title'}`} />
+          <Link to={`/users/${userNic}`}>
+            <p className="photo__autor-page-link">
+              { 'Autor\'s page link' }
+            </p>
+          </Link>
+          <a className="photo__autor-link" href={userPortfolioUrl}>{ 'Autor\'s portfolio link' }</a>
+        </Card>
+      )}
+      { !isPhotoLoading && requestError && (
+        <p>
+          Такое фото не найдено.
+          {' '}
+          <Link to="/">
+            Перейти на главную.
+          </Link>
+        </p>
+      )}
+    </div>
+  );
+});
+// class Photo extends PureComponent {
+//   componentDidMount = () => {
+//     const { match, photoRequestAction: handleAction } = this.props;
+//     handleAction(match);
+//   };
 
-  render() {
-    const {
-      isPhotoLoading,
-      photoSrc,
-      userPortfolioUrl,
-      userNic,
-      photoDesc,
-      requestError,
-    } = this.props;
+//   render() {
+//     const {
+//       isPhotoLoading,
+//       photoSrc,
+//       userPortfolioUrl,
+//       userNic,
+//       photoDesc,
+//       requestError,
+//     } = this.props;
 
-    return (
-      <div className="photo-container photo">
-        { isPhotoLoading && (<Spinner className="spinner" />)}
-        { !isPhotoLoading && !requestError && (
-          <Card
-            style={{ width: '100%' }}
-            cover={(
-              <img
-                className="photo__img"
-                alt="example"
-                src={photoSrc}
-              />
-            )}
-          >
-            <Meta className="photo__desc" title={`${photoDesc || 'No title'}`} />
-            <Link to={`/users/${userNic}`}>
-              <p className="photo__autor-page-link">
-                { 'Autor\'s page link' }
-              </p>
-            </Link>
-            <a className="photo__autor-link" href={userPortfolioUrl}>{ 'Autor\'s portfolio link' }</a>
-          </Card>
-        )}
-        { !isPhotoLoading && requestError && (
-          <p>
-            Такое фото не найдено.
-            {' '}
-            <Link to="/">
-              Перейти на главную.
-            </Link>
-          </p>
-        )}
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="photo-container photo">
+//         { isPhotoLoading && (<Spinner className="spinner" />)}
+//         { !isPhotoLoading && !requestError && (
+//           <Card
+//             style={{ width: '100%' }}
+//             cover={(
+//               <img
+//                 className="photo__img"
+//                 alt="example"
+//                 src={photoSrc}
+//               />
+//             )}
+//           >
+//             <Meta className="photo__desc" title={`${photoDesc || 'No title'}`} />
+//             <Link to={`/users/${userNic}`}>
+//               <p className="photo__autor-page-link">
+//                 { 'Autor\'s page link' }
+//               </p>
+//             </Link>
+//             <a className="photo__autor-link" href={userPortfolioUrl}>{ 'Autor\'s portfolio link' }</a>
+//           </Card>
+//         )}
+//         { !isPhotoLoading && requestError && (
+//           <p>
+//             Такое фото не найдено.
+//             {' '}
+//             <Link to="/">
+//               Перейти на главную.
+//             </Link>
+//           </p>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
 Photo.propTypes = {
   photoRequestAction: PropTypes.func,
