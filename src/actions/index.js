129f6b3b3d7ca5fb77d67d102a909e097a7619d4
psywalker <psywalker09@gmail.com>
2019-06-18 20:42:39 +0300

    Make constants for actions types (part 2) #67

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,31 @@
+import {
+  LOGOUT,
+  USER_FETCHING,
+  USER_LIKES_FETCHING,
+  USER_PHOTO_LISTING_FETCHING,
+  PHOTO_FETCHING,
+  PHOTO_IMAGE_LOAD,
+  USER_STATISTIC_FETCHING,
+  LOGIN_FETCHING,
+  CARDS_PHOTOS_FETCHING,
+  PAGINATION_CHANGE,
+  FILTER_ITEM_VALUE,
+  SEARCH_TEXT,
+  SEARCH_TEXT_CHANGE_INPUT_VALUE,
+} from '../constants';
+
 export const logoutAction = () => ({
-  type: 'LOGOUT',
+  type: LOGOUT,
 });
 
 export const userRequestAction = match => ({
-  type: 'USER_FETCHING',
+  type: USER_FETCHING,
   isUserFetching: true,
   match,
 });
 
 export const userLikesRequestAction = (userId, page, perPage) => ({
-  type: 'USER_LIKES_FETCHING',
+  type: USER_LIKES_FETCHING,
   isUserLikesFetching: true,
   userId,
   page,
@@ -17,7 +33,7 @@ export const userLikesRequestAction = (userId, page, perPage) => ({
 });
 
 export const userPhotoListingRequestAction = (userId, page, perPage) => ({
-  type: 'USER_PHOTO_LISTING_FETCHING',
+  type: USER_PHOTO_LISTING_FETCHING,
   isUserPhotoListingFetching: true,
   userId,
   page,
@@ -25,50 +41,50 @@ export const userPhotoListingRequestAction = (userId, page, perPage) => ({
 });
 
 export const photoRequestAction = match => ({
-  type: 'PHOTO_FETCHING',
+  type: PHOTO_FETCHING,
   isPhotoLoading: true,
   match,
 });
 export const photoImageLoadAction = () => ({
-  type: 'PHOTO_IMAGE_LOAD',
+  type: PHOTO_IMAGE_LOAD,
 });
 
 export const userStatistingRequestAction = userId => ({
-  type: 'USER_STATISTIC_FETCHING',
+  type: USER_STATISTIC_FETCHING,
   isListingLoading: true,
   userId,
 });
 
 export const loadingRequestAction = location => ({
-  type: 'LOGIN_FETCHING',
+  type: LOGIN_FETCHING,
   fetching: true,
   location,
 });
 
 export const cardsPhotosRequestAction = cardsData => ({
-  type: 'CARDS_PHOTOS_FETCHING',
+  type: CARDS_PHOTOS_FETCHING,
   isListingLoading: true,
   cardsData,
 });
 
 export const paginationChangeAction = currentPage => ({
-  type: 'PAGINATION_CHANGE',
+  type: PAGINATION_CHANGE,
   page: currentPage,
 });
 
 export const filterItemValueAction = (itemText, itemId) => ({
-  type: 'FILTER_ITEM_VALUE',
+  type: FILTER_ITEM_VALUE,
   itemText,
   itemId,
 });
 
 export const searchTextAction = (text, tags) => ({
-  type: 'SEARCH_TEXT',
+  type: SEARCH_TEXT,
   tags,
   text,
 });
 
 export const searchChangeInputValueAction = text => ({
-  type: 'SEARCH_TEXT_CHANGE_INPUT_VALUE',
+  type: SEARCH_TEXT_CHANGE_INPUT_VALUE,
   text,
 });
