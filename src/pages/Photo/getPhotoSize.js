9b9f274b220a7e0f38c3f5dd2a36f4c4839f7f4f
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2019-06-20 14:13:15 +0300

    Fix the algorithm for photo proportions #66

--- src/pages/Photo/getPhotoSize.js
+++ src/pages/Photo/getPhotoSize.js
@@ -10,15 +10,15 @@ const getPhotoSize = (props) => {
       w = Math.floor(widthPhoto / photoContainerWidth);
       h = Math.floor(heightPhoto / w);
 
-      if (h > allowHeight) return { photoWidth: 'auto', photoHeight: `${allowHeight}px` };
-      return { photoWidth: 'auto', photoHeight: `${h}px` };
+      if (h > allowHeight) return { photoWidth: 'auto', photoHeight: `${allowHeight - 60}px` };
+      return { photoWidth: 'auto', photoHeight: `${h - 60}px` };
     }
     if (heightPhoto > widthPhoto) {
       h = Math.floor(heightPhoto / allowHeight);
       w = Math.floor(widthPhoto / h);
 
-      if (w > photoContainerWidth) return { photoWidth: `${photoContainerWidth}px`, photoHeight: 'auto' };
-      return { photoWidth: `${w}px`, photoHeight: 'auto' };
+      if (w > photoContainerWidth) return { photoWidth: `${photoContainerWidth - 60}px`, photoHeight: 'auto' };
+      return { photoWidth: `${w - 60}px`, photoHeight: 'auto' };
     }
   }
   return { photoWidth: '300px', photoHeight: 'auto' };
