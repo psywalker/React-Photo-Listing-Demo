0f9a8955936aa9478d0c5110f9d3cd4676e41bc6
psywalker <psywalker09@gmail.com>
2019-07-03 22:36:56 +0300

    Test login reducer (part 2) #70

--- src/reducers/login.test.js
+++ src/reducers/login.test.js
@@ -1,20 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
 import * as t from '../constants/actionTypes';
+import loginReducer from './login';
 import initialStore from '../initialStore';
-import * as loginReducer from './login';
 
-const initStore = initialStore.login;
+const initLoginState = initialStore.login;
 
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Test of reducer `login`', () => {
 
-  it('Test `logoutAction`', () => {
+  const initialState = {
+    fetching: false,
+    loginError: false,
+  };
 
+  describe('Test of `LOGIN_FETCHING`', () => {
+    it('LOGIN_FETCHING', () => {
+      const action = {
+        type: t.LOGIN_FETCHING,
+      };
+
+      expect(loginReducer(initLoginState, action)).toEqual({
+        ...initLoginState,
+        fetching: true,
+        loginError: false,
+      })
+    });
   });
+  describe('Test of `LOGIN_FETCHING`', () => {
+    it('LOGIN_SUCCESS', () => {
+      const action = {
+        type: 'LOGIN_SUCCESS',
+        dataForProps: {
+          profilePhotoUrl: 'http://net.com',
+          profileName: 'Name',
+          profileFullName: 'FirstName LastName',
+          profileEmail: 'psywalker09@gmail.com',
+        }
+      };
+
+      expect(loginReducer(initLoginState, action)).toEqual({
+        ...initLoginState,
+        ...action.dataForProps,
+        fetching: false,
+        loginError: false,
+      })
+    });
+
+    it('LOGIN_SUCCESS 2', () => {
+      const action = {
+        type: 'LOGIN_SUCCESS',
+      };
+
+      expect(loginReducer(initLoginState, action)).not.toEqual({
+        ...initLoginState,
+        fetching: true,
+      })
+    });
+  });
+
 });
