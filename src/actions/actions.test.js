4cf03b18937ddab688f2ee8a9f9f2eb7b5d00aaf
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2019-07-03 18:35:44 +0300

    Test action `userRequestAction` #69

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -8,16 +8,41 @@ import { createSerializer } from 'enzyme-to-json';
 import * as t from '../constants/actionTypes';
 import {
   logoutAction,
-} from './';
+  userRequestAction,
+} from '.';
 
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Test of actions', () => {
+
   it('Test `logoutAction`', () => {
     const expectedAction = {
       type: t.LOGOUT,
     };
     expect(logoutAction()).toEqual(expectedAction);
+  });
+
+  describe('Test `userRequestAction`', () => {
+
+    it('Test `userRequestAction` with `isUserFetching` egual true (Passed test)', () => {
+      const expectedAction = {
+        type: t.USER_FETCHING,
+        isUserFetching: true,
+        match: { match: {} },
+      };
+      expect(userRequestAction({ match: {} })).toEqual(expectedAction);
+    });
+
+    it('Test `userRequestAction` with `isUserFetching` egual false (Failed test)', () => {
+      const expectedAction = {
+        type: t.USER_FETCHING,
+        isUserFetching: false,
+        match: { match: {} },
+      };
+      expect(userRequestAction({ match: {} })).not.toEqual(expectedAction);
+    });
+
+    
   });
 });
