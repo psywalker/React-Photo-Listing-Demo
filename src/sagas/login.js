d4fdbb43d52fb2df33f4e6f85114b50c5ef8aea3
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2019-07-05 18:31:22 +0300

    Test login saga #71

--- src/sagas/login.js
+++ src/sagas/login.js
@@ -4,12 +4,18 @@ import get from 'lodash/get';
 import { URL_FOR_PROFILE_ME, URL_FOR_TOKEN, URL_FOR_AVATAR_PLACEHOLDER } from '../constants';
 
 let headers = {};
+let axiosRequestForToken = {};
 export const fetchLoginData = () => axios.get(URL_FOR_PROFILE_ME, { headers }).then(response => ({
   profilePhotoUrl: get(response, 'data.profile_image.large', URL_FOR_AVATAR_PLACEHOLDER),
   profileName: get(response, 'data.first_name', ''),
   profileFullName: get(response, 'data.name', ''),
   profileEmail: get(response, 'data.email', ''),
 }));
+
+export const fetchLoginForTokenData = () => (
+  axios.post(axiosRequestForToken.url, axiosRequestForToken.body).then(response => (
+    get(response, 'data.access_token', false)
+  )));
 
 export function* loginAfterToken(token) {
   if (token) {
@@ -22,6 +28,8 @@ export function* loginAfterToken(token) {
     } catch (error) {
       yield put({ type: 'LOGIN_ERROR' });
     }
+  } else {
+    yield put({ type: 'LOGIN_ERROR' });
   }
 }
 
@@ -33,7 +41,7 @@ export function* loginSaga(action) {
     const code = action.location.search.split('?code=')[1];
     if (code) {
       try {
-        const axiosRequestForToken = {
+        axiosRequestForToken = {
           url: URL_FOR_TOKEN,
           body: {
             redirect_uri: process.env.REACT_APP_UNSPLASH_API_REDIRECT_URI,
@@ -44,11 +52,12 @@ export function* loginSaga(action) {
           },
         };
 
-        const response = yield axios.post(axiosRequestForToken.url, axiosRequestForToken.body);
-        const token = get(response, 'data.access_token', false);
+        // const response = yield axios.post(axiosRequestForToken.url, axiosRequestForToken.body);
+        // const token = get(response, 'data.access_token', false);
+        const token = yield call(fetchLoginForTokenData);
         window.localStorage.clear();
         window.localStorage.setItem('token', token);
-        yield loginAfterToken(token);
+        yield call(loginAfterToken, token);
       } catch (error) {
         yield put({ type: 'LOGIN_ERROR' });
       }
--- src/sagas/login.test.js
+++ src/sagas/login.test.js
@@ -8,20 +8,76 @@ import { put, call  } from 'redux-saga/effects';
 import axios from 'axios';
 import * as t from '../constants/actionTypes';
 import initialStore from '../initialStore';
-import { fetchLoginData, loginAfterToken } from './login';
+import { fetchLoginData, loginAfterToken, loginSaga, fetchLoginForTokenData } from './login';
 import { URL_FOR_PROFILE_ME, URL_FOR_TOKEN, URL_FOR_AVATAR_PLACEHOLDER } from '../constants';
 
 const initStore = initialStore.login;
 
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 Enzyme.configure({ adapter: new Adapter() });
+const assert = require('assert');
 
 describe('Test of saga `login`', () => {
-  it('loginAfterToken Saga test', () => {
-    const mockResponse = { profileEmail: 'email@email.com' };
-    const gen = loginAfterToken('tokken');
-    expect(gen.next().value).toEqual(call(fetchLoginData));
-    expect(gen.next(mockResponse).value).toEqual(put({ type: 'LOGIN_SUCCESS', dataForProps: mockResponse }));
-    expect(gen.next()).toEqual({ done: true, value: undefined });
+
+  describe('loginAfterToken Saga test`', () => {
+    it('loginAfterToken Saga test Passed', () => {
+      const mockResponse = { profileEmail: 'email@email.com' };
+      const gen = loginAfterToken('tokken');
+      expect(gen.next().value).toEqual(call(fetchLoginData));
+      expect(gen.next(mockResponse).value).toEqual(put({ type: 'LOGIN_SUCCESS', dataForProps: mockResponse }));
+      expect(gen.next()).toEqual({ done: true, value: undefined });
+    });
+
+    it('loginAfterToken Saga test Failed 1', () => {
+      const gen = loginAfterToken('tokken');
+      gen.next();
+      expect(gen.throw('product not found').value).toEqual(put({ type: 'LOGIN_ERROR' }));
+    });
+
+    it('loginAfterToken Saga test Failed 2', () => {
+      try {
+        const func = () => new Error('ответ не найден в моей базе данных');
+        const gen = loginAfterToken('tokken');
+        expect(gen.next().value).toEqual(call(fetchLoginData));
+        gen.next(func());
+      } catch (error) {
+        expect(error).toEqual(put({ type: 'LOGIN_ERROR' }));
+      }
+    });
+
+    it('loginAfterToken Saga test Failed 3', () => {
+      const gen = loginAfterToken();
+      expect(gen.next().value).toEqual(put({ type: 'LOGIN_ERROR' }));
+    });
   });
+
+  describe('loginSaga Saga test`', () => {
+    it('loginSaga Saga test', () => {
+      const token = '34uukj4349834983';
+      const action = {
+        location: {
+          search: '?code=d3b75c4fdb173c3058a8f1aa80dff6b89b59bf4231cb4183a178edd0c4275c0d',
+        },
+      };
+      const gen = loginSaga(action);
+      expect(gen.next().value).toEqual(call(fetchLoginForTokenData));
+      expect(gen.next(token).value).toEqual(call(loginAfterToken, token));
+
+    });
+
+    it('loginSaga Saga test if tokenFirst when `yield loginAfterToken(tokenFirst)`', () => {
+      // const mockResponse = '34uukj4349834983';
+      // const tokenFirst = '34uukj4349834983';
+      // const action = {
+      //   location: {
+      //     search: '?code=d3b75c4fdb173c3058a8f1aa80dff6b89b59bf4231cb4183a178edd0c4275c0d',
+      //   },
+      // };
+      // const gen = loginSaga(action);
+      // expect(gen.next().value).toEqual(call(fetchLoginForTokenData));
+      // expect(gen.next(mockResponse).value).toEqual(loginAfterToken(mockResponse));
+
+    });
+  });
+
 });
