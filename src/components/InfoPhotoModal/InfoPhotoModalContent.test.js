4063bbd0f54ce82619512dcd7ca1f420257cf107
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2019-07-23 14:28:37 +0300

    Test InfoPhotoModalContent component (part 3) #68

--- src/components/InfoPhotoModal/InfoPhotoModalContent.test.js
+++ src/components/InfoPhotoModal/InfoPhotoModalContent.test.js
@@ -42,29 +42,65 @@ describe('Test of component of SmallPhotoListing', () => {
   };
   
   const pagePhotoInfoHeader = 'header[data-test="photoInfoHeader"]';
+  const photoInfoTitle = 'h2[data-test="photoInfoTitle"]';
+  const photoInfoDesc = 'b[data-test="photoInfoDesc"]';
+  const photoInfoDate = 'p[data-test="photoInfoDate"]';
+  const photoInfoContent = 'div[data-test="photoInfoContent"]';
+  const photoInfoListHead = 'ul[data-test="photoInfoListHead"]';
+  const photoInfoListHeadItem = 'li[data-test="photoInfoListHeadItem"]';
+  const photoInfoListHeadTitle = 'h3[data-test="photoInfoListHeadTitle"]';
+  const photoInfoListHeadTextMain = 'p[data-test="photoInfoListHeadTextMain"]';
+  const photoInfoListHeadText = 'p[data-test="photoInfoListHeadText"]';
+  const photoInfoListContent = 'ul[data-test="photoInfoListContent"]';
+  const photoInfoListItem = 'li[data-test="photoInfoListItem"]';
+  const photoInfoListTitle = 'h3[data-test="photoInfoListTitle"]';
+  const photoInfoListText = 'p[data-test="photoInfoListText"]';
+  const photoInfo = 'p[data-test="photoInfo"]';
 
   const appSelector = wrapper => ({
     getPagePhotoInfoHeader: () => wrapper.find(pagePhotoInfoHeader),
-
+    getPagePhotoInfoTitle: () => wrapper.find(photoInfoTitle),
+    getPagePhotoInfoDesc: () => wrapper.find(photoInfoDesc),
+    getPagePhotoInfoDate: () => wrapper.find(photoInfoDate),
+    getPagePhotoInfoContent: () => wrapper.find(photoInfoContent),
+    getPagePhotoListHead: () => wrapper.find(photoInfoListHead),
+    getPagePhotoListHeadItem: () => wrapper.find(photoInfoListHeadItem),
+    getPagePhotoListHeadTitle: () => wrapper.find(photoInfoListHeadTitle),
+    getPagePhotoListHeadTextMain: () => wrapper.find(photoInfoListHeadTextMain),
+    getPagePhotoListHeadText: () => wrapper.find(photoInfoListHeadText),
+    getPagePhotoInfoListContent: () => wrapper.find(photoInfoListContent),
+    getPagePhotoInfoListItem: () => wrapper.find(photoInfoListItem),
+    getPagePhotoInfoListTitle: () => wrapper.find(photoInfoListTitle),
+    getPagePhotoInfoListText: () => wrapper.find(photoInfoListText),
+    getPagePhotoInfo: () => wrapper.find(photoInfo),
   });
 
-  const propses = {
-    ...propsFull,
-  };
-  const infoPhotoModalContent = global.mountWrap(<InfoPhotoModalContent {...propses} />);
-  console.log(infoPhotoModalContent.debug());
+  // const propses = {
+  //   ...propsFull,
+  // };
+  // const infoPhotoModalContent = global.mountWrap(<InfoPhotoModalContent {...propses} />);
+  // console.log(infoPhotoModalContent.debug());
 
   describe('InfoPhotoModal component initial', () => {
     it('renders without initial props', () => {
-      // const infoPhotoModal = global.mountWrap(<InfoPhotoModal {...propsFull} />);  
-      // const page = appSelector(infoPhotoModal);
-      // const photoModalContent = setInfoPhotoModalContent(propsFull);
+      const infoPhotoModalContent = global.mountWrap(<InfoPhotoModalContent />);
+      const page = appSelector(infoPhotoModalContent);
 
-      // const photoInfoWrap = page.getPagePhotoInfoWrap();
-      // const openModalBtn = page.getPageOpenModalBtn();
-      // const openModalBtnIcon = page.getPageOpenModalBtnIcon();
-      // const modal = page.getPageModal();
-
+      const infoHeader = page.getPagePhotoInfoHeader();
+      const infoTitle = page.getPagePhotoInfoTitle();
+      const infoDesc = page.getPagePhotoInfoDesc();
+      const infoDate = page.getPagePhotoInfoDate();
+      const infoContent = page.getPagePhotoInfoContent();
+      const listHead = page.getPagePhotoListHead();
+      const listHeadItem = page.getPagePhotoListHeadItem();
+      const listHeadTitle = page.getPagePhotoListHeadTitle();
+      const listHeadTextMain = page.getPagePhotoListHeadTextMain();
+      const listHeadText = page.getPagePhotoListHeadText();
+      const listContent = page.getPagePhotoInfoListContent();
+      const listItem = page.getPagePhotoInfoListItem();
+      const listTitle = page.getPagePhotoInfoListTitle();
+      const listText = page.getPagePhotoInfoListText();
+      const info = page.getPagePhotoInfo();
     });
   });
 });
