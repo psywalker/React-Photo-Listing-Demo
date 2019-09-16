825e0bc5b737c5c2e6bfcfc6635d0c49825b17b1
psywalker <psywalker09@gmail.com>
2019-09-16 22:34:49 +0300

    more test

--- src/router/index.js
+++ src/router/index.js
@@ -9,13 +9,9 @@ import {
   HeaderApp,
 } from '../components';
 import HomeHOC from '../pages/Home/HomeHOC';
-import Test from '../pages/Test';
 import './index.scss';
 
 const Main = memo(() => (
-  // <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
-  //   <Route exact path="/test" component={props => <Test {...props} />} />
-  // </BrowserRouter>
   <BrowserRouter basename={process.env.PUBLIC_URL || '/'}>
     <LastLocationProvider>
       <Row className="header-row" style={{ margin: '0px 0 30px' }}>
@@ -32,7 +28,6 @@ const Main = memo(() => (
           <Route path="/profile" component={props => <Profile {...props} />} />
           <Route path="/users/:id" component={props => <User {...props} />} />
           <Route path="/:tag" component={props => <HomeHOC {...props} />} />
-          {/* <Route exact path="/test" component={Test} /> */}
         </Switch>
       </div>
     </LastLocationProvider>
