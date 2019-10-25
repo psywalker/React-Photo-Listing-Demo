c100eeac205f5b2a178df1827d278228718f9370
Maksim Usachev <m.usachev@nb0306.bizone.bi.zone>
2019-10-25 17:16:12 +0300

    Updates

---
+++ precache-manifest.eede422904fc87cee7691bad8de31170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a113a88d4d0c286f7c9ce90f472dab40",
+    "url": "/React-Photo-Listing-Demo/index.html"
+  },
+  {
+    "revision": "2a88efdda2ea35a73a55",
+    "url": "/React-Photo-Listing-Demo/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "f54d30111ddb0ae89da0",
+    "url": "/React-Photo-Listing-Demo/static/css/main.6b9cd06e.chunk.css"
+  },
+  {
+    "revision": "2a88efdda2ea35a73a55",
+    "url": "/React-Photo-Listing-Demo/static/js/2.1b4fed80.chunk.js"
+  },
+  {
+    "revision": "f54d30111ddb0ae89da0",
+    "url": "/React-Photo-Listing-Demo/static/js/main.54fe75bc.chunk.js"
+  },
+  {
+    "revision": "71c74a5ce564bebb6e87",
+    "url": "/React-Photo-Listing-Demo/static/js/runtime~main.d39bbc17.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Photo-Listing-Demo/precache-manifest.e849cd9fa204d91234c4ef13ac0d2e67.js"
+  "/React-Photo-Listing-Demo/precache-manifest.eede422904fc87cee7691bad8de31170.js"
 );
 
 self.addEventListener('message', (event) => {
