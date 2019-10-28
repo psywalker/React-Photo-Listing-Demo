ea98bce1f67cb9079facb4105f2e9fcdce8dff50
psywalker <psywalker09@gmail.com>
2019-10-28 10:07:03 +0300

    Make it possible to put likes #101

--- src/components/LikesButton/index.js
+++ src/components/LikesButton/index.js
@@ -1,5 +1,5 @@
-import React, { memo, useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { memo, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
   Button,
@@ -8,31 +8,12 @@ import {
 import {
   photoLike,
   photoUnlike,
-  getPhoto,
 } from '../../actions';
 import './index.scss';
 
 const LikesButton = memo(({ photoID }) => {
   const [like, setLike] = useState(false);
-  const login = useSelector(state => state.login);
   const dispatch = useDispatch();
-  const { profileName } = login;
-  if (!profileName) return null;
-  const getphoto = useSelector(state => state.getphoto);
-  const { liked } = getphoto;
-  console.log("777.1: ", like);
-  console.log("777.2: ", liked);
-  useEffect(() => {
-    dispatch(getPhoto(photoID));
-    console.log("777.3: ", liked);
-  }, [photoID]);
-
-  useEffect(() => {
-    if (liked) setLike(true);
-    else setLike(false);
-    console.log("777.4: ", like);
-    console.log("777.5: ", like);
-  }, [liked]);
 
   const likePhoto = () => {
     dispatch(photoLike(photoID));
--- src/components/PhotoCardList/PhotoCard/index.js
+++ src/components/PhotoCardList/PhotoCard/index.js
@@ -1,9 +1,9 @@
 import React, { memo } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import PhotoCardImg from '../PhotoCardImg';
 import PhotoCardTags from '../PhotoCardTags';
 import PhotoCardAutor from '../PhotoCardAutor';
-import PhotoCardDescription from '../PhotoCardDescription';
 import LikesButton from '../../LikesButton';
 import DownLoadButton from '../../DownLoadButton';
 import './index.scss';
@@ -11,40 +11,40 @@ import './index.scss';
 const PhotoCard = memo(({
   onSearchTagValue,
   item,
-}) => (
-  <li className="photo-card-list-item photo-card-list__item">
+}) => {
+  const login = useSelector(state => state.login);
+  const { profileName } = login;
+  return (
+    <li className="photo-card-list-item photo-card-list__item">
 
-    <PhotoCardAutor
-      userID={item.userID}
-      userAvatar={item.userAvatar}
-      title={item.title}
-    />
+      <PhotoCardAutor
+        userID={item.userID}
+        userAvatar={item.userAvatar}
+        title={item.title}
+      />
 
-    <LikesButton photoID={item.photoID} />
+      { profileName && <LikesButton photoID={item.photoID} /> }
 
-    <PhotoCardImg
-      photoID={item.photoID}
-      photoAltDesc={item.photoAltDesc}
-      photoName={item.photoName}
-    />
+      <PhotoCardImg
+        photoID={item.photoID}
+        photoAltDesc={item.photoAltDesc}
+        photoName={item.photoName}
+      />
 
-    {/* <PhotoCardDescription
-      photoDesc={item.photoDesc}
-    /> */}
+      <PhotoCardTags
+        onSearchTagValue={onSearchTagValue}
+        tags={item.tags}
+      />
 
-    <PhotoCardTags
-      onSearchTagValue={onSearchTagValue}
-      tags={item.tags}
-    />
+      <DownLoadButton
+        altDescriprion={item.photoAltDesc}
+        photoDesc={item.photoDesc}
+        photoSrc={item.photoName}
+      />
 
-    <DownLoadButton
-      altDescriprion={item.photoAltDesc}
-      photoDesc={item.photoDesc}
-      photoSrc={item.photoName}
-    />
-
-  </li>
-));
+    </li>
+  );
+});
 
 
 PhotoCard.propTypes = {
