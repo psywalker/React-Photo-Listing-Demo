9e04c890a101bdcf8d93e229e6ee607e75a07eff
psywalker <psywalker09@gmail.com>
2019-11-24 14:57:23 +0000

    Updates

--- precache-manifest.052df0de4d8bd9d219c3cafdb1cb1c30.js
+++ precache-manifest.052df0de4d8bd9d219c3cafdb1cb1c30.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cbf5b6c4cfcec1b3f969794f6672e2db",
+    "revision": "90bc5fcfbbed68bba308a5debb28acee",
     "url": "/React-Photo-Listing-Demo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Photo-Listing-Demo/static/css/2.46439c26.chunk.css"
   },
   {
-    "revision": "47847b1f5ebe5c21d125",
+    "revision": "16f1686f5d848b909913",
     "url": "/React-Photo-Listing-Demo/static/css/main.78968832.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Photo-Listing-Demo/static/js/2.472ef33d.chunk.js"
   },
   {
-    "revision": "47847b1f5ebe5c21d125",
-    "url": "/React-Photo-Listing-Demo/static/js/main.59a30986.chunk.js"
+    "revision": "16f1686f5d848b909913",
+    "url": "/React-Photo-Listing-Demo/static/js/main.20efb2a2.chunk.js"
   },
   {
     "revision": "71c74a5ce564bebb6e87",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Photo-Listing-Demo/precache-manifest.de6d87ca36f0279b44f22773846497d7.js"
+  "/React-Photo-Listing-Demo/precache-manifest.052df0de4d8bd9d219c3cafdb1cb1c30.js"
 );
 
 self.addEventListener('message', (event) => {
