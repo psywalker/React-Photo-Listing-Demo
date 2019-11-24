2d89a550c4995562ae299ad3a4c3453c8e360555
psywalker <psywalker09@gmail.com>
2019-11-24 17:01:21 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Photo-Listing-Demo/precache-manifest.446e0d9e20cb3d699467164380a59f65.js"
+  "/React-Photo-Listing-Demo/precache-manifest.052df0de4d8bd9d219c3cafdb1cb1c30.js"
 );
 
 self.addEventListener('message', (event) => {
