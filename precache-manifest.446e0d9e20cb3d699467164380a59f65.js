ba07ba007eca6663e7f69bd289b9eeab77d39d8a
psywalker <psywalker09@gmail.com>
2019-11-24 16:52:59 +0000

    Updates

--- precache-manifest.446e0d9e20cb3d699467164380a59f65.js
+++ precache-manifest.446e0d9e20cb3d699467164380a59f65.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "90bc5fcfbbed68bba308a5debb28acee",
+    "revision": "f438eb33455887983fbe0cb5ff76cce7",
     "url": "/React-Photo-Listing-Demo/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Photo-Listing-Demo/static/css/2.46439c26.chunk.css"
   },
   {
-    "revision": "16f1686f5d848b909913",
-    "url": "/React-Photo-Listing-Demo/static/css/main.78968832.chunk.css"
+    "revision": "8f3c756f404f2657286b",
+    "url": "/React-Photo-Listing-Demo/static/css/main.c8e5d641.chunk.css"
   },
   {
     "revision": "6cfe235dda18c952ae1d",
     "url": "/React-Photo-Listing-Demo/static/js/2.472ef33d.chunk.js"
   },
   {
-    "revision": "16f1686f5d848b909913",
+    "revision": "8f3c756f404f2657286b",
     "url": "/React-Photo-Listing-Demo/static/js/main.20efb2a2.chunk.js"
   },
   {
