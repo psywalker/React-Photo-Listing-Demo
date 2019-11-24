45ba90deabbe81bd53500fcdfb337dc7eb9fdd7f
psywalker <psywalker09@gmail.com>
2019-11-24 16:21:40 +0000

    Updates

---
+++ precache-manifest.995a27f5eaad6141ac498a7954ef0e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a05a51833fe65395d87ad287134168",
+    "url": "/React-Photo-Listing-Demo/index.html"
+  },
+  {
+    "revision": "a142dcd0a33a7f5b78eb",
+    "url": "/React-Photo-Listing-Demo/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "c209dc64cfaeda12f16a",
+    "url": "/React-Photo-Listing-Demo/static/css/main.78968832.chunk.css"
+  },
+  {
+    "revision": "a142dcd0a33a7f5b78eb",
+    "url": "/React-Photo-Listing-Demo/static/js/2.4a867c3c.chunk.js"
+  },
+  {
+    "revision": "c209dc64cfaeda12f16a",
+    "url": "/React-Photo-Listing-Demo/static/js/main.017b51cb.chunk.js"
+  },
+  {
+    "revision": "71c74a5ce564bebb6e87",
+    "url": "/React-Photo-Listing-Demo/static/js/runtime~main.d39bbc17.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Photo-Listing-Demo/precache-manifest.052df0de4d8bd9d219c3cafdb1cb1c30.js"
+  "/React-Photo-Listing-Demo/precache-manifest.995a27f5eaad6141ac498a7954ef0e0e.js"
 );
 
 self.addEventListener('message', (event) => {
